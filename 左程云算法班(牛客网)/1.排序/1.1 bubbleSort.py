d49c1ac53d6bf54fff8b4231f93502e6f4918abd
yuanjun <861022418@qq.com>
2020-12-25 18:38:22 +0800

    修改部分代码

--- 左程云算法班(牛客网)/1.排序/1.1 bubbleSort.py
+++ 左程云算法班(牛客网)/1.排序/1.1 bubbleSort.py
@@ -1,18 +1,24 @@
 def bubbleSort(arr):
     """冒泡排序"""
+    # 方式1: (外层索引从末尾开始取)外层索引 i 依次递减，内层索引递增 i
+    n = len(arr)
+    for i in range(n - 1, -1, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                swap(arr, j, j + 1)
+
+
+def bubbleSort(arr):
+    """冒泡排序"""
+    # 方式2: (外层索引从0开始取)外层索引 i 依次递增，内层索引递增到 arr.length - i - 2
     n = len(arr)
     for i in range(n):
         for j in range(0, n - i - 1):
-            if arr[j] > arr[j+1]:
-                swap(arr, i, j)
+            if arr[j] > arr[j + 1]:
+                swap(arr, j, j + 1)
 
 
 def swap(arr, i, j):
     arr[i] = arr[i] ^ arr[j]
     arr[j] = arr[i] ^ arr[j]
     arr[i] = arr[i] ^ arr[j]
-
-
-def comparator(arr):
-    arr.sort()
-

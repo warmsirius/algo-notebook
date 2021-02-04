258736d91bc92c2ae43c10e55f320c5426f9f870
yuanjun <861022418@qq.com>
2021-02-04 17:41:34 +0800

    1个栈实现另1个栈的排序

--- 刷题/1.栈和队列/5.用1个栈实现另1个栈的排序/Solution.py
+++ 刷题/1.栈和队列/5.用1个栈实现另1个栈的排序/Solution.py
@@ -2,11 +2,20 @@ class SortStackByStack(object):
     """用1个栈实现另1个栈的排序"""
     def __init__(self, stack):
         self.stack = stack
-        self.help = []
+        # _help: 从大到小保存stack中的值
+        self._help = []
+
         while self.stack:
-            cur = self.stack.pop()
-            while self.help and (help[-1] > cur):
-                self.stack.append(self.help.pop())
-            self.help.append(cur)
-        while self.help:
-            self.stack.append(self.help.pop())
+            cur_value = self.stack.pop()
+            while self._help and cur_value > self._help[-1]:
+                self.stack.append(self._help.pop())
+            self._help.append(cur_value)
+        # _help: 依次弹出，从小到大
+        while self._help:
+            self.stack.append(self._help.pop())
+
+
+if __name__ == "__main__":
+    stack = [4, 3, 1, 2, 1, 0]
+    sort_stack_by_stack = SortStackByStack(stack)
+    assert stack == [0, 1, 1, 2, 3, 4]

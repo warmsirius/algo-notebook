08ffbb5e1bfe5a092b8b6b4c385067e6c1257dc1
yuanjun <2859626066@qq.com>
2021-02-15 17:25:41 +0800

    max-min<=num的情况

--- 刷题/1.栈和队列/程序员代码面试指南/8.最大值减去最小值小于或等于num的子数量数目/Solution.py
+++ 刷题/1.栈和队列/程序员代码面试指南/8.最大值减去最小值小于或等于num的子数量数目/Solution.py
@@ -17,16 +17,27 @@ def getNum(arr, num):
             while len(qmin) and qmin[-1] >= arr[j]:
                 qmin.pop()
             qmin.append(j)
+
             while len(qmax) and qmax[-1] <= arr[j]:
                 qmax.pop()
             qmax.append(j)
+
+            # max-min > num，i往右移动1位，当前循环退出
             if arr[qmax[0]] - arr[qmin[0]] > num:
                 break
+            # 如果满足条件，那就j继续往右移动
             j += 1
+
+        # i应该往右移动1位，将qmax、qmin中的i删除
         if qmin[0] == i:
             qmin.popleft()
         if qmax[0] == i:
             qmax.popleft()
+        # res = (j-1) - i + 1 = j - i
         res += j - i
         i += 1
     return res
+
+
+if __name__ == '__main__':
+    getNum([], 1)
